0def9879a3b578cc8be64ce113bcaa8352fdd5f9
Anton Sukhanov <antonsukhanov@users.noreply.github.com>
2022-02-19 23:52:44 +0300

    Added flight update message producing

--- FlightMicroservices.WebApi/Controllers/FlightUpdatesController.cs
+++ FlightMicroservices.WebApi/Controllers/FlightUpdatesController.cs
@@ -1,5 +1,9 @@
+using System.Text.Json;
+using Confluent.Kafka;
 using FlightMicroservices.WebApi.Models;
 using Microsoft.AspNetCore.Mvc;
+
+// ReSharper disable BuiltInTypeReferenceStyle
 
 namespace FlightMicroservices.WebApi.Controllers;
 
@@ -8,15 +12,18 @@ namespace FlightMicroservices.WebApi.Controllers;
 [Route("api/v1/flight-updates")]
 public class FlightUpdatesController : ControllerBase
 {
+    private const string TopicName = "flight-updates";
     private readonly ILogger<FlightUpdatesController> _logger;
+    private readonly IProducer<Null, String> _producer;
 
-    public FlightUpdatesController(ILogger<FlightUpdatesController> logger)
+    public FlightUpdatesController(ILogger<FlightUpdatesController> logger, IProducer<Null, String> producer)
     {
         _logger = logger;
+        _producer = producer;
     }
 
     /// <summary>
-    /// Processes new flight update report.
+    /// Processes new flight update report
     /// </summary>
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
@@ -25,10 +32,20 @@ public class FlightUpdatesController : ControllerBase
     public async ValueTask<IActionResult> ProcessFlightUpdate([FromBody] FlightUpdateDto flightUpdateDto)
     {
         _logger.LogInformation(
-            "Received update for flight '{FlightNumber}' (new departure date '{DepartureDate}') from host '{Host}'",
-            flightUpdateDto.FlightNumber, flightUpdateDto.DepartureDate,
+            "[{TraceIdentifier}] Received update for flight '{FlightNumber}' (new departure date '{DepartureDate}') from host '{Host}'",
+            HttpContext.TraceIdentifier, flightUpdateDto.FlightNumber, flightUpdateDto.DepartureDate,
             HttpContext.Connection.RemoteIpAddress?.ToString() ?? "UNKNOWN_HOST");
 
+        var message = new Message<Null, String>
+        {
+            Value = JsonSerializer.Serialize(flightUpdateDto)
+        };
+        
+        var result = await _producer.ProduceAsync(TopicName, message);
+        
+        _logger.LogInformation("[{TraceIdentifier}] Published flight update report to topic '{TopicName}' with offset '{MessageOffset}'",
+            HttpContext.TraceIdentifier, TopicName, result.Offset.Value);
+        
         return NoContent();
     }
 }
