6266f52c362de7d2de90d7644e83c3bce7c4ccd9
Anton Sukhanov <antonsukhanov@users.noreply.github.com>
2022-02-20 01:12:10 +0300

    Updated namespaces

--- FlightMicroservices.WebApi/Controllers/FlightUpdatesController.cs
+++ FlightMicroservices.WebApi/Controllers/FlightUpdatesController.cs
@@ -1,6 +1,6 @@
 using System.Text.Json;
 using Confluent.Kafka;
-using FlightMicroservices.WebApi.Models;
+using FlightMicroservices.WebApi.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 // ReSharper disable BuiltInTypeReferenceStyle
--- FlightMicroservices.WebApi/Models/Dto/FlightUpdateDto.cs
+++ FlightMicroservices.WebApi/Models/Dto/FlightUpdateDto.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 // ReSharper disable AutoPropertyCanBeMadeGetOnly.Global
 // ReSharper disable UnusedAutoPropertyAccessor.Global
 
-namespace FlightMicroservices.WebApi.Models;
+namespace FlightMicroservices.WebApi.Models.Dto;
 
 public class FlightUpdateDto
 {
