72ff8aa2d08f98fecbf713a1580705e30bfc7e43
Anton Sukhanov <antonsukhanov@users.noreply.github.com>
2022-02-19 22:49:21 +0300

    Set up swagger docs

--- FlightMicroservices.WebApi/Program.cs
+++ FlightMicroservices.WebApi/Program.cs
@@ -1,25 +1,38 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Flight Updates API",
+        Description = "API consumes flight update reports",
+        Contact = new OpenApiContact
+        {
+            Name = "Anton Sukhanov",
+            Email = string.Empty,
+        }
+    });
+    
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+// Configure middlewares.
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.MapControllers();
 
 app.Run();
